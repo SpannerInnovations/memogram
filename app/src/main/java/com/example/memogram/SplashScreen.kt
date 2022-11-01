d2a031bc2c483a38bcb35dc796dee8bcc50c2596
ivanenriquez <frost.ivan06@gmail.com>
2022-11-01 16:09:04 -0400

    Fixed issue with the SplashScreen.

--- app/src/main/java/com/example/memogram/SplashScreen.kt
+++ app/src/main/java/com/example/memogram/SplashScreen.kt
@@ -4,14 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.view.WindowManager
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 
 @Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+//
+
 
         // This is used to hide the status bar and make
         // the splash screen as a full screen activity.
@@ -20,12 +25,19 @@ class SplashScreen : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
+
+        fun showGif() {
+            val imageView: ImageView = findViewById(androidx.appcompat.R.id.spacer)
+            Glide.with(this).load(R.drawable.giphy).into(imageView)
+        }
+
         // we used the postDelayed(Runnable, time) method
         // to send a message with a delayed time.
         //Normal Handler is depricated , so we have to change the code little bit
 
         // Handler().postDelayed({
         Handler(Looper.getMainLooper()).postDelayed({
+
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
