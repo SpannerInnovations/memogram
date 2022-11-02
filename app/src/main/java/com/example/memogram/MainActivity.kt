899a42b0e27209f2f9c5b381f2651e01828cfafc
ivanenriquez <frost.ivan06@gmail.com>
2022-11-02 06:20:20 -0400

    UserHome implemented

--- app/src/main/java/com/example/memogram/MainActivity.kt
+++ app/src/main/java/com/example/memogram/MainActivity.kt
@@ -9,23 +9,31 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
+    lateinit var firstName : EditText;
+    lateinit var topscreenimage : ImageView;
+    lateinit var passwordEditText : EditText;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        topscreenimage = findViewById(R.id.topscreenimage);
+        Glide.with(this).load(R.drawable.nebulous).into(topscreenimage);
 
-        val topscreenimage : ImageView = findViewById(R.id.topscreenimage);
-        val firstName : EditText = findViewById(R.id.username);
-        val passwordEditText : EditText = findViewById(R.id.password);
+
+
+        firstName  = findViewById(R.id.username);
+        passwordEditText   = findViewById(R.id.password);
         passwordEditText.transformationMethod = AsteriskTransformationMethod();
-        Glide.with(this).load(R.drawable.nebulous).into(topscreenimage)
 
 
 
-        fun enterBtn(view: View) {
-            val intent = Intent(this@MainActivity,UserHome::class.java)
-            intent.putExtra("firstname", firstName.text.toString());
-            startActivity(intent)
-        }
 
+
+
+    }
+    fun loginBtn(view: View) {
+        val intent = Intent(this@MainActivity,UserHome::class.java)
+        intent.putExtra("firstname", firstName.text.toString());
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/memogram/UserHome.kt
+++ app/src/main/java/com/example/memogram/UserHome.kt
@@ -2,10 +2,35 @@ package com.example.memogram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 
 class UserHome : AppCompatActivity() {
+    lateinit var name : TextView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_home)
+
+        name = findViewById(R.id.homeusername);
+        name.text = intent.getStringExtra("firstname")
+
+        //creating the action bar and show the back arrow
+        val actionBar: ActionBar? = supportActionBar
+        // calling the action bar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+
+    //method called when click on back arrow
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
