dca4ece3a437c72f149ff0286c33b8552da82804
hbiga <hbigatto@gmail.com>
2022-11-02 12:48:27 -0400

    textView fixed

--- app/src/main/java/com/example/memogram/UserHome.kt
+++ app/src/main/java/com/example/memogram/UserHome.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 
-
 class UserHome : AppCompatActivity() {
 
     lateinit var name : TextView;
